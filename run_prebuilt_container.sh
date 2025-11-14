f1ca65c032f064f94e02869c368283e7a7880d5d
factoryal-marcopanato <144120192+factoryal-marcopanato@users.noreply.github.com>
2025-11-14 11:27:54 +0100

    Added hostname to make prebuilt container work

--- run_prebuilt_container.sh
+++ run_prebuilt_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker network create xlayer-network --subnet 192.168.213.0/24
-docker run --name xlayer-container -d --network xlayer-network --ip 192.168.213.10 --privileged -p 80:80 -p 443:443 -p 6443:6443 xlayer-docker:20251113
+docker run --name xlayer-container -d --network xlayer-network --ip 192.168.213.10 --privileged -p 80:80 -p 443:443 -p 6443:6443 -h 23802c9929bf xlayer-docker:20251113
